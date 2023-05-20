94735d352fd31c5010e2ca412f3b672dab760d24
fawpc <42224153+fawpc@users.noreply.github.com>
2023-05-20 17:20:09 -0300

    Delete LittleTasksWebAPI/EndPoints directory

--- LittleTasksWebAPI/EndPoints/Profiles/ProfileRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LittleTasksWebAPI.EndPoints.Profiles
-{
-    public class ProfileRequest
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
--- LittleTasksWebAPI/EndPoints/Profiles/ProfileResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LittleTasksWebAPI.EndPoints.Profiles
-{
-    public class ProfileResponse
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
